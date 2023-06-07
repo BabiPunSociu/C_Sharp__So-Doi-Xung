09d58ef74e38eb413cd7675d861b6f1b2a2c8906
BabiPunSociu <Nguyendung281002@gmail.com>
2023-06-07 15:55:34 +0700

    DungNguyen

---
+++ KiemTra.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoDoiXung
+{
+    internal class KiemTra
+
+    {
+        public static bool check(int a)
+        {
+            int a_cpy = a;
+            int a_doiCho = 0;
+            while(a_cpy > 0)
+            {
+                a_doiCho = a_doiCho * 10 + (a_cpy % 10);
+                a_cpy /= 10;
+            }
+            return (a_doiCho == a)? true : false;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoDoiXung
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            do
+            {
+                Console.Write("Nhap so nguyen duong a: a = ");
+                a = Convert.ToInt32(Console.ReadLine());
+            }while(a <= 0);
+
+            Console.WriteLine("\n" + a + " là số đối xứng?\t" + KiemTra.check(a));
+        }
+    }
+}
